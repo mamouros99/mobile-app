4ad9eb8373ffb4042f6296c9b76e31afca7b413e
mamouros99 <tiago.mamouros@gmail.com>
2023-02-27 15:07:16 +0000

    Start of Mobile App

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button class="btn">
+      Make a Report
+    </button>
   </div>
 </template>
 
